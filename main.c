f48332dcc64db85d8efad15648edd0eaabf82d18
LalaBritta <randrianalisoa.lbl@gmail.com>
2025-10-13 16:16:17 +0200

    test avec horloge

--- main.c
+++ main.c
@@ -1,22 +1,62 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
-#define LED     PD5
-#define BOUTON  PB5
+#define LED4 PD4
+#define LED5 PD5
+#define LED6 PD6
+#define LED7 PD7
 
+#define BOUTON4 PB4
+#define BOUTON5 PB5
+#define BOUTON6 PB6
 
-// PD => DDRD ou PORTD
-// PB => DDRB ou PORTB
-// -vvv : pour voir plus d'informations
+int main(void)
+{
 
-int main(void){
-  DDRD |= (1 << LED);         // Sortie pour la LED
-  DDRB &= ~(1 << BOUTON);     // Entrée pour le bouton
-  PORTB |= (1 << BOUTON);     // Activation de la résistance de tirage interne pour le bouton
+  CLKPR = 0b10000000; // modification du diviseur d'horloge (CLKPCE=1)
+  CLKPR = 0;          // modification du diviseur d'horloge (CLKPCE=1)
 
-  while(1){
-      if(PINB & (1 << BOUTON)) 
-          PORTD &= ~(1 << LED);  // LED éteinte si bouton relâché
-      else 
-          PORTD |= (1 << LED);   // LED allumée si bouton appuyé
+  // LED4
+  DDRD |= (1 << LED4);
+  DDRB &= ~(1 << BOUTON4);
+  PORTB |= (1 << BOUTON4);
+
+  // LED5
+  DDRD |= (1 << LED5);
+  DDRB &= ~(1 << BOUTON5);
+  PORTB |= (1 << BOUTON5);
+
+  // LED6
+  DDRD |= (1 << LED6);
+  DDRB &= ~(1 << BOUTON6);
+  PORTB |= (1 << BOUTON6);
+
+  while (1)
+  {
+    #if 0
+        // Bouton 4 → LED4
+        if(PINB & (1 << BOUTON4))  
+          PORTD &= ~(1 << LED4); 
+        else                      
+          PORTD |= (1 << LED4);
+
+        // Bouton 5 → LED5
+        if(PINB & (1 << BOUTON5))  
+          PORTD &= ~(1 << LED5); 
+        else                      
+          PORTD |= (1 << LED5);
+
+        // Bouton 6 → LED6
+        if(PINB & (1 << BOUTON6))  
+          PORTD &= ~(1 << LED6); 
+        else                      
+          PORTD |= (1 << LED6);
+
+    #endif
+    PORTD |= (1 << LED6);
+
+    _delay_ms(1000);
+    PORTD &= ~(1 << LED6);
+    _delay_ms(1000);
   }
 }
